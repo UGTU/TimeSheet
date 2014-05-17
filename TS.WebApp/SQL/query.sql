30a97fbcfe299f4ad28c4930c13f570178c2a1ed
lehays <lehays@rambler.ru>
2014-05-17 12:20:19 +0400

    1

--- TS.WebApp/SQL/query.sql
+++ TS.WebApp/SQL/query.sql
@@ -5,3 +5,18 @@ select DISTINCT tsr.idTimeSheet, tsr.idFactStaffHistory  from ShemaTabel.TimeShe
 
 select DISTINCT tsr.idTimeSheet, tsr.idFactStaffHistory  from ShemaTabel.TimeSheetRecord tsr where tsr.idTimeSheet=802
 
+
+
+-------------------------------------------------------------------------------------------------------------------------------------
+select*from ShemaTabel.TimeSheet
+alter table ShemaTabel.TimeSheet drop column ApproveStep
+alter table ShemaTabel.TimeSheet drop column idWF
+select*from ShemaTabel.TimeSheet
+
+select*from ShemaTabel.TimeSheetRecord
+alter table ShemaTabel.TimeSheetRecord drop column IsChecked
+select*from ShemaTabel.TimeSheetRecord
+
+ALTER TABLE ShemaTabel.TimeSheetRecord
+ADD {COLUMNNAME} {TYPE} {NULL|NOT NULL} 
+CONSTRAINT {CONSTRAINT_NAME} DEFAULT {DEFAULT_VALUE}
