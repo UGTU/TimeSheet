2d99ca4e9c1fa859ca09716e9d15bc7279aa4a68
iulyashev <iulyashev@IVANUL-PC>
2016-09-19 10:37:53 +0300

    --исправление в GetDepartmentsList

--- TS.WebApp/TimeSheetService.svc.cs
+++ TS.WebApp/TimeSheetService.svc.cs
@@ -321,7 +321,7 @@ namespace TimeSheetMvc4WebApplication
                 return
                     db.Approver.Where(
                         w => w.idDepartment == idDepartment & w.ApproverType.ApproveNumber == approveNumber &
-                             ((w.DateEnd == null) ||(w.DateEnd >= DateTime.Today))).
+                             ((w.DateEnd == null) ||(w.DateEnd >= DateTime.Now))).
                         Select(s => DtoClassConstructor.DtoApprover(db, s.Employee.id,isAdmin)).FirstOrDefault();
             }
         }
