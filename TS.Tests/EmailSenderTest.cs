896dd29792aa2e4cde7bde73295faa59abb509e6
ulyashevivan <ulyashevivan@gmail.com>
2017-10-06 09:50:13 +0300

    добавлены тетсты по отправке писем

---
+++ TS.Tests/EmailSenderTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimeSheetMvc4WebApplication.Source;
+
+namespace TimeSheetMvc4WebApplication.Tests
+{
+    [TestClass]
+    public class EmailSenderTest
+    {
+        [TestMethod]
+        public async void emailSent()
+        {
+            var appDominUrl = ".";//Url.Action("Index", null, null, Request.Url.Scheme);
+            var TES = new TimeEmailSender("mail.ugtu.net",
+                        appDominUrl);
+            Assert.IsTrue(await TES.SendMail("tabel-no-reply@ugtu.net", "iulyashev@ugtu.net", "test", "body", true));
+        }
+    }
+}
---
+++ TS.Tests/UnitTest2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Mail;
+
+namespace TimeSheetMvc4WebApplication.Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void emailSent()
+        {
+            MailAddress from = new MailAddress("tabel-no-reply@ugtu.net");
+            MailAddress to = new MailAddress("ngrigoriev@ugtu.net");
+            MailMessage m = new MailMessage(from, to);
+            m.Subject = "Тест";
+            m.Body = "<h2>Здорова колян</h2>";
+            m.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient("mail.ugtu.net");
+            smtp.Send(m);
+
+            //var appDominUrl = ".";//Url.Action("Index", null, null, Request.Url.Scheme);
+            //var TES = new TimeEmailSender("mail.ugtu.net",
+            //            appDominUrl);
+            //TES.SendMail("iulyashev@ugtu.net", "ngrigoriev@ugtu.net", "test", "body", true);
+        }
+    }
+}
